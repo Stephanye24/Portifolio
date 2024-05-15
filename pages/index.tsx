809a68a68a064d98bc96ecc4b71f923465a570ba
Stephanye <stephanye.vasconcellos@yssy.com.br>
2024-05-15 15:56:29 -0300

    feat(iniciando o projeto): adicionado componente generico de box para iniciar o projeto

---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+import Box from "../src/components/Box";
+import theme from "../src/theme";
+
+export default function HomeScreen() {
+  return <Box  tag="main"  styleSheet={{fontFamily: theme.typography.fontFamily}}    >oi</Box>;
+}
